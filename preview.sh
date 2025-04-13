ecc33e9454256330292ebf4d4aa8c6970709fd3c
iceyrazor <iceyfoxservers@gmail.com>
2025-04-13 12:43:28 -0500

    fixed preview.sh when first char in ytid is - such as -yK

--- preview.sh
+++ preview.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 rowid="$(echo $1 | sed 's/|.*//g')"
-thumbnail="$(ls thumbnails/ | grep "$(sqlite3 ./youtube_stuffs.db "select id from ytlist where rowid=$rowid;").*" )"
+thumbnail="$(ls thumbnails/ | grep -e "$(sqlite3 ./youtube_stuffs.db "select id from ytlist where rowid=$rowid;").*" )"
 printf "thumbnails/$thumbnail"
